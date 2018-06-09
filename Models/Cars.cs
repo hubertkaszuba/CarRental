cfecb5a25394a0f0153b87216f5bb6df2ff18934
Michal <michalg10.mg@gmail.com>
2018-06-09 22:44:14 +0200

    dodany spis samochodów

--- Models/Cars.cs
+++ Models/Cars.cs
@@ -14,7 +14,7 @@ namespace CarRental.Models
         public string Mark { get; set; }
         public string Model { get; set; }
         public string Category { get; set; }
-        public bool? Available { get; set; }
+        public bool Available { get; set; }
 
         public ICollection<Rent> Rent { get; set; }
     }
