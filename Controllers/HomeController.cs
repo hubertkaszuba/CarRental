6bf6f3a1638219bd010511f5884bec96e60e5cc1
hubertkaszuba <hubert.kaszuba@gmail.com>
2018-06-09 20:56:44 +0200

    Login page as a default page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CarRental.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CarRental.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
